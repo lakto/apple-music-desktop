b17765c07465e8c3206caa7a430295fcca2ce339
André Kilchenmann <github@milchkannen.ch>
2024-10-29 11:21:34 +0100

    chore(linux): setting for deb

--- forge.config.js
+++ forge.config.js
@@ -1,48 +1,50 @@
 const path = require('path');
 
 module.exports = {
-  packagerConfig: {
-    asar: true,
-    executableName: 'youtube-desktop',
-    icon: './images/icon',
+  'packagerConfig': {
+    'asar': true,
+    'executableName': 'youtube-desktop',
+    'productName': 'YouTube Desktop',
+    'icon': path.resolve(__dirname, 'images', 'icon'),
   },
-  rebuildConfig: {},
-  makers: [
+  'rebuildConfig': {},
+  'makers': [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        iconUrl: 'https://raw.githubusercontent.com/mikepruett3/youtube-desktop/main/images/YouTube.ico',
-        setupIcon: './images/icon.ico'
+      'name': '@electron-forge/maker-squirrel',
+      'config': {
+        'iconUrl': 'https://raw.githubusercontent.com/mikepruett3/youtube-desktop/main/images/YouTube.ico',
+        'setupIcon': './images/icon.ico'
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      'name': '@electron-forge/maker-deb',
+      'config': {
+        'options': {
+          'productName': 'YouTube Desktop',
+          'icon': path.resolve(__dirname, 'images', 'icon.png')
+        }
+      }
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+      'name': '@electron-forge/maker-zip',
+      'platforms': ['darwin'],
+    }
   ],
-  publishers: [
+  'publishers': [
     {
-      name: '@electron-forge/publisher-github',
-      config: {
-        repository: {
-          owner: 'mikepruett3',
-          name: 'youtube-desktop'
+      'name': '@electron-forge/publisher-github',
+      'config': {
+        'repository': {
+          'owner': 'mikepruett3',
+          'name': 'youtube-desktop'
         }
       }
     }
   ],
-  plugins: [
+  'plugins': [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
+      'name': '@electron-forge/plugin-auto-unpack-natives',
+      'config': {},
     },
   ],
 };
